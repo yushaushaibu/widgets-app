104dbb6bc6d799d7f4129308167f0015e5808011
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-09 13:30:48 +0000

    Default search term implemented

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,19 +2,22 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    const search = async() => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
           srsearch: term,
-        }
+        },
       });
+
+      setResults(data.query.search);
     };
 
     search();
