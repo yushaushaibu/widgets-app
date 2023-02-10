7f677e1dd9964456b86cc286f32597af87c4be47
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-10 10:06:39 +0000

    Adds button and linked it to Wikipedia pages

--- src/components/Search.js
+++ src/components/Search.js
@@ -26,6 +26,9 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+      <div className="right floated content">
+        <a href={`https://en.wikipedia.org?curid=${result.pageid}`} className="ui button">Go</a>
+      </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
