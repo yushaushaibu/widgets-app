131a540733a6d97e061580f490d263ccabcf9035
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-17 11:02:47 +0000

    Adding controlled input field

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ options, selected, onSelectedChange, label }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
@@ -37,7 +37,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   return (
     <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a Color</label>
+        <label className="label">{label}</label>
         <div
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
           onClick={() => setOpen(!open)}
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -2,25 +2,41 @@ import React, { useState } from "react";
 import Dropdown from "./Dropdown";
 
 const options = [
-    {
-        label: 'Afrikaans',
-        value: 'af'
-    },
-    {
-        label: 'Arabic',
-        value: 'ar'
-    },
-    {
-        label: 'Hindi',
-        value: 'hi'
-    }
+  {
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    label: "Arabic",
+    value: "ar",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
 ];
 
 const Translate = () => {
-    const [language, setLanguage] = useState(options[0]);
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
   return (
     <div>
-      <Dropdown selected={language} onSelectedChange={setLanguage} options={options}/>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input
+            className="input"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+      </div>
+      <Dropdown
+        label="Select a Language"
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
     </div>
   );
 };
