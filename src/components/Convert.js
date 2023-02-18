f352287c4738e4e7513c787aaae4365299155cb6
yushaushaibu <yushaushaibu1@gmail.com>
2023-02-18 23:01:01 +0000

    Showing translated text on screen

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,21 +2,32 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
-  useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-        },
-      }
-    );
-  }, [language, text]);
+  const [translated, setTranslated] = useState("");
 
-  return <div></div>;
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [text, language]);
+
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
